1bd4cb55f31ba31d2a50089997de34ae8665cf88
Freddy <freddyamarante14@gmail.com>
2021-10-25 19:48:19 -0500

    Activate cors

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -5,34 +5,33 @@ import UsersController from 'App/Controllers/Http/UsersController'
 const baseRoute = 'users'
 
 Route.post('login', async ({ auth, request, response }) => {
-    const email = request.input('email')
-    const password = request.input('password')
-  
-    try {
-      const token = await auth.use('api').attempt(email, password)
-      return token
-    } catch {
-      return response.badRequest('Invalid credentials')
-    }
-  })
-  
+  const email = request.input('email')
+  const password = request.input('password')
 
-Route.post(baseRoute, async ({ request }) => {
-    let userData = request.body()
-    return new UsersController().create(userData)
+  try {
+    const token = await auth.use('api').attempt(email, password)
+    return {
+      user: auth.user,
+      token: token.token,
+    }
+  } catch {
+    return response.badRequest('Invalid credentials')
+  }
 })
 
+Route.post(baseRoute, async ({ request }) => {
+  let userData = request.body()
+  return new UsersController().create(userData)
+})
 
 Route.get('checklogin', async ({ auth }) => {
   await auth.use('api').authenticate()
-  return auth.user!
+  return { user: auth.user }
 })
 
 Route.post('/logout', async ({ auth, response }) => {
   await auth.use('api').revoke()
   return {
-    revoked: true
+    revoked: true,
   }
 })
-
-
